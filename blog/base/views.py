830aab7a1bcef009bb8806f6ecde1ad380d12532
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2021-07-12 15:00:39 +0200

    base setup

---
+++ blog/base/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+class BaseView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'base/index.html')
